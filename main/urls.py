c38311f96140d227fc80b7e0c2d147e66208662a
Steve Sharp <76664627+Steve-Basharov@users.noreply.github.com>
2021-02-19 17:48:50 +0300

    Delete main directory

--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('about_us', views.about, name='about'),
-    path('job_work', views.job, name='job'),
-]
